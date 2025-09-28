68e88ff16c40237c44c74568f08fe745a19ada6f
unel <enee.prog@gmail.com>
2025-09-28 09:23:31 +0200

    create router and add i18n

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
-import { createApp } from "vue";
 import "@/styles/shared.scss";
+import { createApp } from "vue";
 
+import { createMemoryHistory, createRouter } from "vue-router";
 import App from "./App.vue";
+import ConstructPage from "./pages/constructPage.vue";
+import HomePage from "./pages/homePage.vue";
+import { createI18n } from "vue-i18n";
 
-createApp(App).mount("#app");
+import frMessages from "./locales/fr.json";
+import enMessages from "./locales/en.json";
+
+const routes = [
+  { path: "/", component: HomePage },
+  { path: "/form", component: ConstructPage },
+  { path: "/end", component: HomePage },
+];
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+const i18n = createI18n({
+  locale: "fr",
+  fallbackLocale: "fr",
+  messages: {
+    fr: frMessages,
+    en: enMessages,
+  },
+});
+
+app.use(i18n);
+app.use(router);
+app.mount("#app");
---
+++ src/pages/homePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ t("welcome-home-page") }}</h1>
+  <p>{{ t("home-presentation") }}</p>
+  <span class="home-button-wrapper">
+    <UiLink to="/form">{{ t("compete-form") }}</UiLink>
+    <UiLink to="/share">{{ t("share-form") }}</UiLink>
+    <UiLink to="/result">{{ t("see-result") }}</UiLink>
+  </span>
+</template>
+
+<script setup lang="ts">
+import UiLink from "@/components/ui/uiLink.vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+</script>
+
+<style scoped lang="scss">
+h1 {
+  margin: 0 auto;
+}
+
+p {
+  margin: 2vh;
+}
+.home-button-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3vh;
+}
+</style>
