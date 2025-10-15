a3aafb20a1aa0345dffba7d259c6751ff0f98e63
unel <enee.prog@gmail.com>
2025-10-15 22:11:52 +0200

    language selector

--- src/components/footerBar.vue
+++ src/components/footerBar.vue
@@ -1,17 +1,28 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div className="Footer">
-    <span>
+  <div class="Footer">
+    <span class="left">
       <img src="@/assets/logoWhite.png" alt="Logo du Galéromètre" width="100" />
+      <select v-model="$i18n.locale">
+        <option
+          v-for="locale in $i18n.availableLocales"
+          :key="`locale-${locale}`"
+          :value="locale"
+        >
+          {{ locale }}
+        </option>
+      </select>
     </span>
     <span>
-      <span className="FooterElement">
+      <span class="FooterElement">
         Un projet de
         <a target="_blank" href="https://linktr.ee/coordination.etudiante">
           La Coordination étudiante
         </a>
       </span>
       <br />
-      <span className="FooterElement">Réalisé avec ❤️ depuis Grenoble</span>
+      <span class="FooterElement">Réalisé avec ❤️ depuis Grenoble</span>
       <br />
 
       <a target="_blank" href="https://github.com/ThePiratePhone/galerometre/">
@@ -32,6 +43,14 @@
   justify-content: space-between;
   gap: 3vh;
   font-size: 0.8em;
+
+  .left {
+    gap: 10px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
 }
 
 a {
